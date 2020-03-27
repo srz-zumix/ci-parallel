f535a348a6fa8285fb5d2a29414a965bcdf91743
srz_zumix <zumix.cpp@gmail.com>
2020-03-27 23:09:45 +0900

    github actions

---
+++ .ci/report.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+BASEDIR=$(dirname $0)
+
+echo ${PARALLEL_NO}
+
+# DATE=$(TZ="Asia/Tokyo" date)
+DATE=$(date -u)
+. ${BASEDIR}/name.sh $1
+echo $CI_NAME
+
+. ${BASEDIR}/commit-hash.sh
+echo $GIT_COMMIT
+
+if [ -z ${INTEGROMAT_WEBHOOK_URL} ]; then
+  export INTEGROMAT_WEBHOOK_URL="https://hook.integromat.com/k1c42tq981hsvjxtqvoe8runsyfem197"
+fi
+
+curl \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"time\": \"${DATE}\", \"ci\": \"${CI_NAME}\", \"commit\": \"${GIT_COMMIT}\", \"number\":\"${PARALLEL_NO}\" }" \
+  ${INTEGROMAT_WEBHOOK_URL}
