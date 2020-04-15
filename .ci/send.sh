b2008f9e176e8f3fe45bb9149462408244c58fa1
srz_zumix <zumix.cpp@gmail.com>
2020-04-15 10:27:45 +0900

    depends_on clone

--- .ci/send.sh
+++ .ci/send.sh
@@ -7,17 +7,16 @@ if [ "$PLATFORM" = "windows" ]; then
     DATE=$(date -u -Iseconds)
 fi
 
-if [ ! -z ${DRYRUN+x} ]; then
+if [ -z ${DRYRUN+x} ]; then
+    curl \
+        -H "Content-Type: application/json" \
+        -X POST \
+        -d "{\"time\": \"${DATE}\", \"ci\": \"${CI_NAME}\", \"commit\": \"${GIT_COMMIT}\", \"id\": \"${SPREADSHEET_ID}\", \"name\":\"$REPORT_JOB_NAME\", \"event\":\"$1\", \"type\":\"${TYPE}\", \"number\":\"${PARALLEL_NO}\" }" \
+        ${INTEGROMAT_WEBHOOK_URL}
+else
     echo curl \
-    -H "Content-Type: application/json" \
-    -X POST \
-    -d "{\"time\": \"${DATE}\", \"ci\": \"${CI_NAME}\", \"commit\": \"${GIT_COMMIT}\", \"id\": \"${SPREADSHEET_ID}\", \"name\":\"$REPORT_JOB_NAME\", \"event\":\"$1\", \"type\":\"${TYPE}\", \"number\":\"${PARALLEL_NO}\" }" \
-    ${INTEGROMAT_WEBHOOK_URL}
-    return
+        -H "Content-Type: application/json" \
+        -X POST \
+        -d "{\"time\": \"${DATE}\", \"ci\": \"${CI_NAME}\", \"commit\": \"${GIT_COMMIT}\", \"id\": \"${SPREADSHEET_ID}\", \"name\":\"$REPORT_JOB_NAME\", \"event\":\"$1\", \"type\":\"${TYPE}\", \"number\":\"${PARALLEL_NO}\" }" \
+        ${INTEGROMAT_WEBHOOK_URL}
 fi
-
-curl \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d "{\"time\": \"${DATE}\", \"ci\": \"${CI_NAME}\", \"commit\": \"${GIT_COMMIT}\", \"id\": \"${SPREADSHEET_ID}\", \"name\":\"$REPORT_JOB_NAME\", \"event\":\"$1\", \"type\":\"${TYPE}\", \"number\":\"${PARALLEL_NO}\" }" \
-  ${INTEGROMAT_WEBHOOK_URL}
