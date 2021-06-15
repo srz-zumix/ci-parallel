d725f5e89f26e0fc005062733e7dd97f0a6d9a85
srz_zumix <zumix.cpp@gmail.com>
2021-06-15 10:32:55 +0900

    up

--- .ci/report.sh
+++ .ci/report.sh
@@ -4,7 +4,7 @@ BASEDIR=$(dirname $0)
 
 . ${BASEDIR}/os-detect.sh
 
-. ${BASEDIR}/name.sh $1
+. ${BASEDIR}/ci_env.sh
 echo $CI_NAME
 
 if [ -z ${PARALLEL_NO+x} ]; then
@@ -26,9 +26,6 @@ echo ${REPORT_JOB_NAME}
 if [ -z ${SLEEP_SECONDS+x} ]; then
   export SLEEP_SECONDS=180
 fi
-
-. ${BASEDIR}/commit-hash.sh
-echo $GIT_COMMIT
 
 if [ -z ${INTEGROMAT_WEBHOOK_URL} ]; then
   export INTEGROMAT_WEBHOOK_URL="https://hook.integromat.com/k1c42tq981hsvjxtqvoe8runsyfem197"
